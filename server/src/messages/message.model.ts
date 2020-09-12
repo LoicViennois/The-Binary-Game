24576e097d710ebc12b1464d1b1311739acf3e97
Loïc Viennois <contact@loicviennois.com>
2020-09-12 19:02:35 +0200

    (server) remove server files

--- server/src/messages/message.model.ts
+++
@@ -1,6 +0,0 @@
-import { User } from '../users/user.model'
-
-export interface Message {
-  sender: User,
-  text: string
-}
--- server/src/users/user.model.ts
+++
@@ -1,7 +0,0 @@
-export interface User {
-  id: string,
-  socketId: string,
-  name: string,
-  playing: boolean,
-  inParty: boolean,
-}
