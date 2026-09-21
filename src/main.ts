e7f0cad1a1ed94ee70a7b741bb04f8d27482be64
Loïc Viennois <contact@loicviennois.com>
2026-09-21 21:30:40 +0200

    refactor: replace deprecated platformBrowserDynamic with platformBrowser

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { isDevMode, provideZoneChangeDetection } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
+platformBrowser().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .then(() => {
     if ('serviceWorker' in navigator && !isDevMode()) {
       navigator.serviceWorker.register('./ngsw-worker.js').then();
